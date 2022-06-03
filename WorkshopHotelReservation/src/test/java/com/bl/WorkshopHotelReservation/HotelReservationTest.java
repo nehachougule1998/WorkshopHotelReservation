02d9526453d184c0529043ac21ac60f457a27aa5
nehachougule1998 <nchougule734@gmail.com>
2022-06-03 13:30:31 +0530

    WeekDayWeekEndRates

--- WorkshopHotelReservation/src/test/java/com/bl/WorkshopHotelReservation/HotelReservationTest.java
+++ WorkshopHotelReservation/src/test/java/com/bl/WorkshopHotelReservation/HotelReservationTest.java
@@ -5,18 +5,27 @@ import org.junit.Test;
 
 public class HotelReservationTest {
 
-	@Test
-	public void givenHotelNamesWhenAddedShouldReturnSize() {
-		HotelReservationSystem obj = new HotelReservationSystem();
-		obj.addHotel();
-		Assert.assertEquals(3,obj.hotelReservation.size());
-	}
-	
-	
-	 @Test
-	    public void givenDateRangeShouldReturnTheCheapestHotelRate() {
-	        HotelReservationSystem  obj = new HotelReservationSystem();
-	        obj.addHotel();
-	        Assert.assertEquals(220, obj.findCheapestHotel("2020-09-10", "2020-09-11"));
-	    }
-	}
+    @Test
+    public void givenHotelNamesWhenAddedShouldReturnSize() {
+        HotelReservationSystem obj = new HotelReservationSystem();
+        obj.addHotel();
+        Assert.assertEquals(3, obj.hotelReservation.size());
+    }
+    
+    @Test
+    public void givenDateRangeShouldReturnTheCheapestHotelRate() {
+        HotelReservationSystem  obj = new HotelReservationSystem();
+        obj.addHotel();
+        Assert.assertEquals(220, obj.findCheapestHotel("2020-09-10", "2020-09-11"));
+    }
+    
+    @Test
+    public void givenWeekDayWeekEndRatesShouldReturnThoseRates() {
+        HotelReservationSystem obj = new HotelReservationSystem();
+        obj.addHotel();
+        Assert.assertEquals(90, obj.hotelReservation.get("Lakewood").getWeekendRate());
+        Assert.assertEquals(50, obj.hotelReservation.get("Bridgewood").getWeekendRate());
+        Assert.assertEquals(150, obj.hotelReservation.get("Ridgewood").getWeekendRate());
+    }
+
+}
