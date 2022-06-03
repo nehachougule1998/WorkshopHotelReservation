088a41b303cb6207299099574972b7867ccebeaf
nehachougule1998 <nchougule734@gmail.com>
2022-06-03 13:03:08 +0530

    AbilityToFindCheapestHotel

--- WorkshopHotelReservation/src/test/java/com/bl/WorkshopHotelReservation/HotelReservationTest.java
+++ WorkshopHotelReservation/src/test/java/com/bl/WorkshopHotelReservation/HotelReservationTest.java
@@ -11,4 +11,12 @@ public class HotelReservationTest {
 		obj.addHotel();
 		Assert.assertEquals(3,obj.hotelReservation.size());
 	}
-}
+	
+	
+	 @Test
+	    public void givenDateRangeShouldReturnTheCheapestHotelRate() {
+	        HotelReservationSystem  obj = new HotelReservationSystem();
+	        obj.addHotel();
+	        Assert.assertEquals(220, obj.findCheapestHotel("2020-09-10", "2020-09-11"));
+	    }
+	}
